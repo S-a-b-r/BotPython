7a18aa53da0c4c49e6ec2325eb42cb500c8f7edd
Sabr <57406194+S-a-b-r@users.noreply.github.com>
2020-10-13 19:03:27 +0400

    Text button

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 BOT_TOKEN = "991756796:AAE2VFvgr03W6d41Tt376IoYNo1aE1kX87U"
 admin_id = 351854729
+URL_APPLES = "https://www.avito.ru/izhevsk?q=яблоки"
+URL_PEAR = "https://www.avito.ru/izhevsk?q=груши"
